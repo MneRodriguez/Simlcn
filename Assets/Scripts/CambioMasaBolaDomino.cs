aa75b46527a8a19ebd71598205c60a5447521786
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-12-02 16:45:00 -0300

    Initial commit

---
+++ Assets/Scripts/CambioMasaBolaDomino.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CambioMasaBolaDomino : MonoBehaviour
+{
+    public Rigidbody rbDeLaBolitaDomino;
+    public float valorMassPorDefecto = 1f;
+
+    public Slider sliderMassBolita;
+    void Start()
+    {
+        rbDeLaBolitaDomino = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void AjustarMassBolitaDomino()
+    {
+        valorMassPorDefecto = sliderMassBolita.value;
+        rbDeLaBolitaDomino.mass = valorMassPorDefecto;
+    }
+}
---
+++ Assets/Scripts/CargarScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CargarScene : MonoBehaviour
+{
+    //public Button btnAbrirPantallaCreds;
+
+    //public TransDeEscns tomarMetodoTransicionarEntreEscenas;
+           
+
+    /*private void Start()
+    {
+        tomarMetodoTransicionarEntreEscenas = GetComponent<TransDeEscns>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CargaScene();
+        }
+    }
+
+    public void CargaScene()
+    {
+        tomarMetodoTransicionarEntreEscenas.TransicionarEntreEscenas();
+    }*/
+
+    public void CargaScene(string nombreEscenaDeJuego)
+    {
+       SceneManager.LoadScene(nombreEscenaDeJuego);
+    }
+
+    public void SalirDelJuego()
+    {
+        Application.Quit();
+    }
+    
+}
