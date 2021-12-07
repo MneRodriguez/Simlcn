d2328d605706549d84d26966d9984da342f1af1d
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-12-07 17:28:41 -0300

    Añadido slider para regular masa de piezas del domino

--- Assets/Scripts/CambioMasaBolaDomino.cs
+++ Assets/Scripts/CambioMasaBolaDomino.cs
@@ -7,15 +7,20 @@ public class CambioMasaBolaDomino : MonoBehaviour
 {
     public Rigidbody rbDeLaBolitaDomino;
     public float valorMassPorDefecto = 0f;
-
     public Slider sliderMassBolita;
-
     public Text txtValorMasaBolita;
+
+    public Rigidbody rbDeLasPiezasDomino;
+    public float valorMassPiezasDominoPorDefecto = 0f;
+    public Slider sliderMassPiezasDomino;
+    public Text txtValorMasaPiezasDomino;
     void Start()
     {
         rbDeLaBolitaDomino = GetComponent<Rigidbody>();
-
         txtValorMasaBolita = GetComponent<Text>();
+
+        rbDeLasPiezasDomino = GetComponent<Rigidbody>();
+        txtValorMasaPiezasDomino = GetComponent<Text>();
                 
     }
 
@@ -23,11 +28,19 @@ public class CambioMasaBolaDomino : MonoBehaviour
     void Update()
     {
         txtValorMasaBolita.text = "Masa de la bola = " + sliderMassBolita.value;
+
+        txtValorMasaPiezasDomino.text = "Masa de las piezas del dominó = " + sliderMassPiezasDomino.value;
     }
 
     public void AjustarMassBolitaDomino()
     {
         valorMassPorDefecto = sliderMassBolita.value;
         rbDeLaBolitaDomino.mass = valorMassPorDefecto;
+    }
+
+    public void AjustarMassPiezasDomino()
+    {
+        valorMassPiezasDominoPorDefecto = sliderMassPiezasDomino.value;
+        rbDeLasPiezasDomino.mass = valorMassPiezasDominoPorDefecto;
     }
 }
