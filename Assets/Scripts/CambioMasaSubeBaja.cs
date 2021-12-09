047413a6f0e5080111b19d9b66f244efb7f78d75
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-12-09 19:27:10 -0300

    Trabajando escena 2

---
+++ Assets/Scripts/CambioMasaSubeBaja.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CambioMasaSubeBaja : MonoBehaviour
+{
+    public Rigidbody rbCuboImpulsador;
+    public float valorMasaCuboImpulsadorPorDefecto = 0f;
+    public Slider sliderMassCuboImpulsador;
+    public Text txtValorMasaCuboImpulsador;
+    // VER SI AGREGO CAMBIO DE SCALE O ROTACION
+
+
+    public Rigidbody rbCuboSiendoImpulsado;
+    public float valorMasaCuboSiendoImpulsadorPorDefecto = 0f;
+    public Slider sliderMassCuboSiendoImpulsado;
+    public Text txtValorMasaCuboSiendoImpulsado;
+    // VER SI AGREGO CAMBIO DE SCALE O ROTACION DE LA RAMPA
+
+
+    public Rigidbody rbRampaSobrePuntoApoyo;
+    public float valorMasaRampaSobrePuntoApoyo = 0f;
+    public Slider sliderMassRampaSobrePuntoApoyo;
+    public Text txtValorMasaRampaSobrePuntoApoyo;
+    // VER SI AGREGO CAMBIO DE SCALE O ROTACION DE LA RAMPA
+
+
+    void Start()
+    {
+        rbCuboImpulsador = GetComponent<Rigidbody>();
+        txtValorMasaCuboImpulsador = GetComponent<Text>();
+
+        rbCuboSiendoImpulsado = GetComponent<Rigidbody>();
+        txtValorMasaCuboSiendoImpulsado = GetComponent<Text>();
+
+    }
+
+    
+    void Update()
+    {
+        txtValorMasaCuboImpulsador.text = "Masa del cubo impulsador = " + sliderMassCuboImpulsador.value;
+
+        txtValorMasaCuboSiendoImpulsado.text = "Masa del cubo siendo impulsado = " + sliderMassCuboSiendoImpulsado.value;
+
+        // VER SI PONGO LO DEL CAMBIO DE SCALE O ROTACION DE LA RAMPA
+    }
+
+    public void AjusteMassCuboImpulsador()
+    {
+        valorMasaCuboImpulsadorPorDefecto = sliderMassCuboImpulsador.value;
+        rbCuboImpulsador.mass = valorMasaCuboImpulsadorPorDefecto;
+        // VER SI AGREGO CAMBIO DE SCALE
+    }
+
+    public void AjusteMassCuboSiendoImpulsado()
+    {
+        valorMasaCuboSiendoImpulsadorPorDefecto = sliderMassCuboSiendoImpulsado.value;
+        rbCuboSiendoImpulsado.mass = valorMasaCuboSiendoImpulsadorPorDefecto;
+        // VER SI AGREGO CAMBIO DE SCALE
+    }
+}
