2aa9c76935e37c708db11cf5a16ab2ce2563f99b
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-12-11 18:09:03 -0300

    Logrado bloquear sliders al arrancar simulacion, escenas 1 y 2

--- Assets/Scripts/InicioSimulacion.cs
+++ Assets/Scripts/InicioSimulacion.cs
@@ -5,21 +5,25 @@ using UnityEngine.UI;
 
 public class InicioSimulacion : MonoBehaviour
 {
+    public CambioMasaBolaDomino tomarSlidersSimulacion1;
+    //public CambioMasaSubeBaja tomarSlidersSimulacion2;
+
+
     public Button BtnIniciarSimulacion;
     private GameObject BotonArrancarSimulacion;
 
     public Button BtnRestartEscenaDomino;
     public Button BtnAbrirSegundaSimulacion;
 
-    public Button BtnRestartEscenaImpulsoRampa;
-    public Button BtnAbrirPrimeraSimulacion;
-    public Button BtnAbrirTerceraSimulacion;
+    //public Button BtnRestartEscenaImpulsoRampa;
+    //public Button BtnAbrirPrimeraSimulacion;
+    //public Button BtnAbrirTerceraSimulacion;
 
     public Text TxtExplicacionDominoEnImgDegrade;
     private GameObject TxtExplicacionDelDomino;
 
-    public Text TxtExplicacionImpulsoEnRampaEnImgDegrade;
-    private GameObject TxtExplicacionDelImpulsoEnRampa;
+    //public Text TxtExplicacionImpulsoEnRampaEnImgDegrade;
+    //private GameObject TxtExplicacionDelImpulsoEnRampa;
 
     public Image ImgDegradeNegroPreSimulacion;
     private GameObject ImgDegradeNegroEnUI; // ERA NECESARIO PARA LUEGO BORRAR EL DEGRADE NEGRO!!!
@@ -31,7 +35,7 @@ public class InicioSimulacion : MonoBehaviour
 
         TxtExplicacionDominoEnImgDegrade = TxtExplicacionDelDomino.GetComponent<Text>();
 
-        TxtExplicacionImpulsoEnRampaEnImgDegrade = TxtExplicacionDelImpulsoEnRampa.GetComponent<Text>();
+        //TxtExplicacionImpulsoEnRampaEnImgDegrade = TxtExplicacionDelImpulsoEnRampa.GetComponent<Text>();
 
         ImgDegradeNegroPreSimulacion = ImgDegradeNegroEnUI.GetComponent<Image>(); // ESTO HACIA FALTA PARA BORRAR EL DEGRADE NEGRO!!!
 
@@ -42,13 +46,14 @@ public class InicioSimulacion : MonoBehaviour
         BtnAbrirSegundaSimulacion = GetComponent<Button>();
 
 
-        BtnRestartEscenaImpulsoRampa = GetComponent<Button>();
-        BtnAbrirPrimeraSimulacion = GetComponent<Button>();
-        BtnAbrirTerceraSimulacion = GetComponent<Button>();
+        //BtnRestartEscenaImpulsoRampa = GetComponent<Button>();
+        //BtnAbrirPrimeraSimulacion = GetComponent<Button>();
+        //BtnAbrirTerceraSimulacion = GetComponent<Button>();
 
 
+        tomarSlidersSimulacion1.sliderMassBolita = GetComponent<Slider>(); // CON REFERENCIAR UNO SOLO DE LOS SLIDERS BASTÓ!!!!
 
-
+        //tomarSlidersSimulacion2.sliderMassCuboImpulsador = GetComponent<Slider>();
     }
 
     
@@ -65,9 +70,17 @@ public class InicioSimulacion : MonoBehaviour
         Destroy(ImgDegradeNegroPreSimulacion);        
         Destroy(BtnIniciarSimulacion.gameObject);
 
-        Destroy(TxtExplicacionImpulsoEnRampaEnImgDegrade);
+        //Destroy(TxtExplicacionImpulsoEnRampaEnImgDegrade);
+
+
+        // DESHABILITAMOS SLIDERS PARA NO HACER MODIFICACIONES SOBRE LA MARCHA, MIENTRAS SE EJECUTA EL ESCENARIO ACTUAL
         
+        tomarSlidersSimulacion1.sliderMassBolita.interactable = false;
+        tomarSlidersSimulacion1.sliderMassPiezasDomino.interactable = false;
 
 
+        //tomarSlidersSimulacion2.sliderMassCuboImpulsador.interactable = false;
+        //tomarSlidersSimulacion2.sliderMassCuboSiendoImpulsado.interactable = false;
+        //tomarSlidersSimulacion2.sliderMassRampaSobrePuntoApoyo.interactable = false;
     }
 }
---
+++ Assets/Scripts/InicioSimulacion2.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InicioSimulacion2 : MonoBehaviour
+{
+    //public CambioMasaBolaDomino tomarSlidersSimulacion1;
+    public CambioMasaSubeBaja tomarSlidersSimulacion2;
+
+
+    public Button BtnIniciarSimulacion;
+    private GameObject BotonArrancarSimulacion;
+
+    public Button BtnRestartEscenaDomino;
+    public Button BtnAbrirSegundaSimulacion;
+
+    public Button BtnRestartEscenaImpulsoRampa;
+    public Button BtnAbrirPrimeraSimulacion;
+    public Button BtnAbrirTerceraSimulacion;
+
+    //public Text TxtExplicacionDominoEnImgDegrade;
+    //private GameObject TxtExplicacionDelDomino;
+
+    public Text TxtExplicacionImpulsoEnRampaEnImgDegrade;
+    private GameObject TxtExplicacionDelImpulsoEnRampa;
+
+    public Image ImgDegradeNegroPreSimulacion;
+    private GameObject ImgDegradeNegroEnUI; // ERA NECESARIO PARA LUEGO BORRAR EL DEGRADE NEGRO!!!
+
+    void Start()
+    {
+        Time.timeScale = 0f; // HABIA QUE PONERLO AL COMIENZO DEL Start, SI NO, LA ESCENA ARRANCABA DIRECTO
+
+
+        //TxtExplicacionDominoEnImgDegrade = TxtExplicacionDelDomino.GetComponent<Text>();
+
+        TxtExplicacionImpulsoEnRampaEnImgDegrade = TxtExplicacionDelImpulsoEnRampa.GetComponent<Text>();
+
+        ImgDegradeNegroPreSimulacion = ImgDegradeNegroEnUI.GetComponent<Image>(); // ESTO HACIA FALTA PARA BORRAR EL DEGRADE NEGRO!!!
+
+
+        BtnIniciarSimulacion = BotonArrancarSimulacion.GetComponent<Button>();
+
+        BtnRestartEscenaDomino = GetComponent<Button>();
+        BtnAbrirSegundaSimulacion = GetComponent<Button>();
+
+
+        BtnRestartEscenaImpulsoRampa = GetComponent<Button>();
+        BtnAbrirPrimeraSimulacion = GetComponent<Button>();
+        BtnAbrirTerceraSimulacion = GetComponent<Button>();
+
+
+        //tomarSlidersSimulacion1.sliderMassBolita = GetComponent<Slider>(); // CON REFERENCIAR UNO SOLO DE LOS SLIDERS BASTÓ!!!!
+
+        tomarSlidersSimulacion2.sliderMassCuboImpulsador = GetComponent<Slider>();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void IniciarSimulacion2()
+    {
+        Time.timeScale = 1f;
+
+        //Destroy(TxtExplicacionDominoEnImgDegrade);
+        Destroy(ImgDegradeNegroPreSimulacion);
+        Destroy(BtnIniciarSimulacion.gameObject);
+
+        Destroy(TxtExplicacionImpulsoEnRampaEnImgDegrade);
+
+
+        // DESHABILITAMOS SLIDERS PARA NO HACER MODIFICACIONES SOBRE LA MARCHA, MIENTRAS SE EJECUTA EL ESCENARIO ACTUAL
+
+        //tomarSlidersSimulacion1.sliderMassBolita.interactable = false;
+        //tomarSlidersSimulacion1.sliderMassPiezasDomino.interactable = false;
+
+
+        tomarSlidersSimulacion2.sliderMassCuboImpulsador.interactable = false;
+        tomarSlidersSimulacion2.sliderMassCuboSiendoImpulsado.interactable = false;
+        tomarSlidersSimulacion2.sliderMassRampaSobrePuntoApoyo.interactable = false;
+    }
+}
